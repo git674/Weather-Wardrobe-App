7cc7fe36799fe7c5bd3a9b154b1a49b2a2e73002
aganapathy1 <aishwaryaganapathy4@gmail.com>
2025-12-01 07:38:53 -0700

    Create OutfitCombos JavaFX application
    Implemented a basic JavaFX application structure for OutfitCombos.

--- Weather Wardrobe App/OutfitCombos.java
+++ Weather Wardrobe App/OutfitCombos.java
@@ -1,2 +1,25 @@
-public class OutfitCombos {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class OutfitCombos extends Application {
+    
+    public void init() {
+        
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        StackPane root = new StackPane();
+        Scene scene = new Scene(root, 800, 600);
+        
+        primaryStage.setTitle("Outfit Combos");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
