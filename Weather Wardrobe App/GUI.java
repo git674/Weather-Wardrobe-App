f6432cff9f8fe59ce8508fad472d973c610fa599
Joshitha Madduri <67445245+git674@users.noreply.github.com>
2025-12-02 08:48:12 -0700

    Refactor GUI class to Interface and update title

--- Weather Wardrobe App/GUI.java
+++ Weather Wardrobe App/GUI.java
@@ -1,25 +1,37 @@
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 
-public class GUI extends Application {
-    
-    public void init() {
-        
+public class Interface extends Application{
+    Button button;
+
+    public static void main(String[] args) {
+        launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        StackPane root = new StackPane();
-        Scene scene = new Scene(root, 800, 600);
-        
-        primaryStage.setTitle("Outfit Combos");
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Weather Wardrobe App");
+
+        button = new Button();
+        button.setText("+");
+
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Button Clicked!");
+            }
+        });
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 300, 250);
         primaryStage.setScene(scene);
         primaryStage.show();
-    }
-    
-    public static void main(String[] args) {
-        launch(args);
     }
 }
